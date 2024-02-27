3de108fe642f7f6d00deacabdaa1d17fcdc11acd
tomwetherell <tomwetherell@icloud.com>
2024-02-27 21:51:48 +0000

    Improve validation prompt and clean ModularRAG class

--- rag.py
+++ rag.py
@@ -27,16 +27,11 @@ AMKEY_TO_UNIT_PATH = "/home/tomw/unifi-pdf-llm/data/AMKEY_unit_conversion.csv"
 client = OpenAI()
 
 RETRIEVE_VALUE_PROMPT_TEMPLATE = """
-Use the following pieces of context to answer the question at the end.
-The answer must be a value from the context.
-The context may be text or a markdown table.
-Just retrieve the answer from the context. Please don't do any unit conversion.
-If you don't know the answer, please return 'null' for the answer and unit.
-Do not return any words other than 'Answer' and 'Unit' in the answer.
-Please return the answer in the format of a python dictionary / JSON object:
-{{"Answer": <number or null>, "Unit": <unit or null>}}
-Please always use double quotes for the keys and values.
-If the requested value is not present in the context, please return 'null' for the answer and unit.
+Use the following markdown tables to as context to answer the question at the end.
+The answer must be a value retrieved directly from the context. Please don't do any unit conversion.
+
+It is possible that the answer is not explicitly stated in the context.
+If it is not possible to retrieve the answer from the context, please return 'null' for the answer and unit.
 
 Context:
 
@@ -54,6 +49,7 @@ Consider the following markdown tables:
 
 Are you sure that '{answer}' is the correct answer to the question: "{question}"?
 
+Please consider each table and each row individually.
 It is possible that the answer is not explicitly stated in the context.
 
 Think step by step. Please conclude your answer with a 'yes' or 'no'.
@@ -85,6 +81,9 @@ class ModularRAG:
         company : str
             The company the documents are for.
 
+        top_k : int
+            The number of documents to retrieve for each query.
+
         amkey_to_metric_path : str
             Path to a csv file mapping AMKEY to metric.
 
@@ -96,37 +95,38 @@ class ModularRAG:
         """
         self.docs = docs
         self.company = company
-        self.top_k = top_k
-        self.amkey_to_metric_path = amkey_to_metric_path
-        self.amkey_to_synonym_path = amkey_to_synonym_path
-        self.amkey_to_unit_path = amkey_to_unit_path
 
+        # Initalised in the methods below
         self.document_store = None
         self.retriever = None
-        self.generation_llm = None
         self.unit_conversion_llm = None
-        self.json_conversion_llm = None
-        self.confirm_relevant_context_llm = None
-        self.amkey_to_metric = None
-        self.amkey_to_synonym = None
-        self.amkey_to_unit = None
+        self.amkey_to_metric_df = None
+        self.amkey_to_synonym_df = None
+        self.amkey_to_unit_df = None
 
-        self.initialise_document_store()
-        self.initialise_retriever()
-        self.initialise_generation_llm()
-        self.initialise_unit_conversion_llm()
-        self.initialise_json_conversion_llm()
-        self.initialise_relevant_context_llm()
-        self.initialise_mappings()
+        self._initialise_document_store()
+        self._initialise_retriever(top_k)
+        self.initialise_unit_conversion_llm()  # TODO: Remove this and use OpenAI API directly, or could the conversion be hardcoded?
+        self._initialise_mappings(
+            amkey_to_metric_path, amkey_to_synonym_path, amkey_to_unit_path
+        )
 
-    def initialise_document_store(self):
+    def _initialise_document_store(self):
         # TODO: Try using other document stores (e.g. FAISS).
         logger.info("Initialising document store")
         self.document_store = InMemoryDocumentStore(embedding_dim=384)
         self.document_store.delete_documents()
         self.document_store.write_documents(self.docs)
 
-    def initialise_retriever(self):
+    def _initialise_retriever(self, top_k: int):
+        """
+        Initialise the retriever.
+
+        Parameters
+        ----------
+        top_k : int
+            The number of documents to retrieve for each query.
+        """
         # TODO: I'm not sure which OpenAI embedding models are available. Is it possible
         # to use their newest embedding models in Haystack v1?
         # TODO: Look into other (non-OpenAI) embedding models that can be used with
@@ -135,21 +135,9 @@ class ModularRAG:
         self.retriever = EmbeddingRetriever(
             embedding_model="sentence-transformers/all-MiniLM-L6-v2",
             document_store=self.document_store,
-            top_k=self.top_k
+            top_k=top_k
         )
         self.document_store.update_embeddings(retriever=self.retriever)
-
-    def initialise_generation_llm(self):
-        # TODO: Temperature = 0 isn't giving deterministic results. Is this a bug?
-        # TODO: Currently 'gpt-3.5-turbo-1106' as it has a larger context window.
-        # There is a newer gpt-3.5 model (gpt-3.5-turbo-0125) which also has a large
-        # context window. I should use that if it's available with Haystack v1.
-        logger.info("Initialising generation LLM")
-        self.generation_llm = PromptNode(
-            model_name_or_path="gpt-3.5-turbo-1106",
-            api_key=OPENAI_API_KEY,
-            model_kwargs={"temperature": 0}
-        )
 
     def initialise_unit_conversion_llm(self):
         logger.info("Initialising unit conversion LLM")
@@ -159,27 +147,30 @@ class ModularRAG:
             model_kwargs={"temperature": 0}
         )
 
-    def initialise_json_conversion_llm(self):
-        logger.info("Initialising json conversion LLM")
-        self.json_conversion_llm = PromptNode(
-            model_name_or_path="gpt-3.5-turbo",
-            api_key=OPENAI_API_KEY,
-            model_kwargs={"temperature": 0}
-        )
+    def _initialise_mappings(
+            self,
+            amkey_to_metric_path: str,
+            amkey_to_synonym_path: str,
+            amkey_to_unit_path: str,
+        ):
+        """
+        Initialise mapping dataframes.
 
-    def initialise_relevant_context_llm(self):
-        logger.info("Initialising relevant context LLM")
-        self.confirm_relevant_context_llm = PromptNode(
-            model_name_or_path="gpt-3.5-turbo",
-            api_key=OPENAI_API_KEY,
-            model_kwargs={"temperature": 0.4}
-        )
+        Parameters
+        ----------
+        amkey_to_metric_path : str
+            Path to a csv file mapping AMKEY to metric.
 
-    def initialise_mappings(self):
+        amkey_to_synonym_path : str
+            Path to a csv file mapping AMKEY and company to metric synonym.
+
+        amkey_to_unit_path : str
+            Path to a csv file mapping AMKEY to desired unit.
+        """
         logger.info("Initialising mappings")
-        self.amkey_to_metric = pd.read_csv(self.amkey_to_metric_path)
-        self.amkey_to_synonym = pd.read_csv(self.amkey_to_synonym_path)
-        self.amkey_to_unit = pd.read_csv(self.amkey_to_unit_path)
+        self.amkey_to_metric_df = pd.read_csv(amkey_to_metric_path)
+        self.amkey_to_synonym_df = pd.read_csv(amkey_to_synonym_path)
+        self.amkey_to_unit_df = pd.read_csv(amkey_to_unit_path)
 
     def query(self, amkey: int, year: int):
         """
@@ -200,6 +191,7 @@ class ModularRAG:
         value : int
             The value associated with the AMKEY for the given year.
         """
+        # Retrieval
         logger.debug(f"Retrieving AMKEY: {amkey}")
         metric = self.retrieve_metric_description(amkey)
         logger.debug(f"Retrieving metric: {metric}")
@@ -210,14 +202,8 @@ class ModularRAG:
 
         try:
             value, unit = self.parse_answer(answer)
-        except json.JSONDecodeError:
-            logger.error(f"Error parsing answer: {answer}")
-            json_conversion_prompt = self.create_json_generation_prompt(answer)
-            answer = self.json_conversion_llm(json_conversion_prompt)[0]
-            logger.debug(f"JSON converted answer: {answer}")
-            value, unit = self.parse_answer(answer)
         except Exception as err:
-            logger.error(f"Non-JSONDecodeError exception when parsing answer: {err}")
+            logger.error(f"Exception raised when parsing answer: {err}")
             value, unit = None, None
 
         logger.debug(f"Parsed answer: {value}, {unit}")
@@ -227,17 +213,18 @@ class ModularRAG:
         # The validator is returning 'no' for quite a few valid answers. Often, it wrongly states
         # that the value is provided for the other years, but not for the year in question.
         unvalidated_value = value
-        if isinstance(value, float):
+        if value is not None:
             logger.debug(f"Validating answer: {value}")
             valid_response = self.validate_response(question, value, context_documents)
             logger.debug(f"Valid response: {valid_response}")
             if valid_response == "no":
                 value, unit = None, None
 
+        # Unit conversion
         required_unit = self.retrieve_unit(amkey)
         logger.debug(f"Required unit: {required_unit}")
 
-        if required_unit is not None and value is not None:
+        if value is not None and required_unit is not None:
             if unit != required_unit:
                 unit_conversion_prompt = self.create_unit_conversion_prompt(value, unit, required_unit)
                 value = self.unit_conversion_llm(unit_conversion_prompt)[0]
@@ -399,14 +386,6 @@ class ModularRAG:
 
         return prompt
 
-    def create_json_generation_prompt(self, answer: str) -> str:
-        prompt=f"""The following answer was generated by a large language model: {answer}.
-                   Please convert this answer to follow the python dictionary / JSON object format:
-                   {{"Answer": <number or null>, "Unit": <unit or null>'}}
-                   \n\n Answer:"""
-
-        return prompt
-
     def parse_answer(self, answer: str) -> tuple[float | None, str | None]:
         """
         Parse the answer returned by the generation LLM.
@@ -415,8 +394,7 @@ class ModularRAG:
         ----------
         answer : str
             The answer returned by the generation LLM. This is expected to be in
-            the format of a python dictionary / JSON object:
-            {"Answer": <number or null>, "Unit": <unit or null>}
+            the format of a JSON object with the keys 'Answer' and 'Unit'.
 
         Returns
         -------
@@ -426,14 +404,10 @@ class ModularRAG:
         unit : str | None
             The unit from the answer.
         """
-        logger.debug(f"Parsing answer: {answer}")
+        answer = json.loads(answer)
 
-        answer_dict = json.loads(answer)
-
-        logger.info(f"answer_dict: {answer_dict}")
-
-        value = answer_dict["Answer"]
-        unit = answer_dict["Unit"]
+        value = answer["Answer"]
+        unit = answer["Unit"]
 
         if value is None:
             pass
@@ -490,9 +464,9 @@ class ModularRAG:
             The company-specific description of the metric, if available.
             Otherwise, None.
         """
-        metric = self.amkey_to_synonym[
-            (self.amkey_to_synonym["AMKEY"] == amkey)
-            & (self.amkey_to_synonym["Group"] == self.company)
+        metric = self.amkey_to_synonym_df[
+            (self.amkey_to_synonym_df["AMKEY"] == amkey)
+            & (self.amkey_to_synonym_df["Group"] == self.company)
         ]["ClientMetric"]
 
         if metric.empty:
@@ -522,8 +496,8 @@ class ModularRAG:
             If the AMKEY is invalid.
         """
         try:
-            metric = self.amkey_to_metric[
-                self.amkey_to_metric["AMKEY"] == amkey
+            metric = self.amkey_to_metric_df[
+                self.amkey_to_metric_df["AMKEY"] == amkey
             ]["ActivityMetric"].item()
         except Exception:
             raise ValueError(f"Invalid AMKEY {amkey}")
@@ -545,8 +519,8 @@ class ModularRAG:
             The required unit for the metric, if specified. Otherwise, None.
         """
         try:
-            unit = self.amkey_to_unit[
-                self.amkey_to_unit["AMKEY"] == amkey
+            unit = self.amkey_to_unit_df[
+                self.amkey_to_unit_df["AMKEY"] == amkey
             ]["Unit"].item()
         except KeyError:
             unit = None
