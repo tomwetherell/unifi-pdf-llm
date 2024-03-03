bdd2b688a6007dcc9b6de972d411b958d1c647f7
tomwetherell <tomwetherell@icloud.com>
2024-03-03 00:02:16 +0000

    Run validation on all 2021 pdfs

--- dev/mapping.py
+++ dev/mapping.py
@@ -3,12 +3,14 @@
 COMPANY_YEAR_PDF_MAPPING = {
     "Absa": {
         2022: ["2022-Absa-Group-limited-Environmental-Social-and-Governance-Data-sheet.pdf"],
+        2021: ["2022-Absa-Group-limited-Environmental-Social-and-Governance-Data-sheet.pdf"],
     },
     "Clicks": {
         2022: ["Clicks-Sustainability-Report-2022.pdf"],
     },
     "Distell": {
         2022: ["DISTELL ESG Appendix 2022.pdf"],
+        2021: ["DISTELL ESG Appendix 2022.pdf"],
     },
     "Impala": {
         2023: ["ESG-spreads.pdf"],
@@ -18,16 +20,19 @@ COMPANY_YEAR_PDF_MAPPING = {
     },
     "Picknpay": {
         2023: ["picknpay-esg-report-spreads-2023.pdf"],
+        2021: ["picknpay-esg-report-spreads-2023.pdf"],
     },
     "Sasol": {
         2023: ["SASOL Sustainability Report 2023 20-09_0.pdf"],
+        2021: ["SASOL Sustainability Report 2023 20-09_0.pdf"],
     },
     "Ssw": {
         2022: ["ssw-IR22.pdf"],
+        2021: ["ssw-IR22.pdf"],
     },
     "Tongaat": {
-        2021: ["2021ESG.pdf"],
-        2020: ["2021ESG.pdf"]
+        2021: ["2021ESG_removed_sup_table.pdf"],
+        2020: ["2021ESG_removed_sup_table.pdf"]
     },
     "Uct": {
         2021: ["UCT_Carbon_Footprint_Report_2020-2021.pdf", "afs2021.pdf"],
