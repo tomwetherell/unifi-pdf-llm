5dfb1f8001c77e71988c559d1fa198939375c7cc
tomwetherell <tomwetherell@icloud.com>
2024-03-16 21:48:40 +0000

    Use function from convert_report.py

--- azure_ai_doc_intelligence/convert_all_reports.py
+++ azure_ai_doc_intelligence/convert_all_reports.py
@@ -1,13 +1,10 @@
 """Script to convert all source pdfs to json using AzureConverter."""
-# TODO: This should call function in convert_report.py
 
 import os
 
-from pathlib import Path
-from loguru import logger
-from haystack.nodes import AzureConverter
 
-from esg_retriever.config import PDF_REPORTS_DIR, JSON_REPORTS_DIR
+from convert_report import convert_report
+from esg_retriever.config import PDF_REPORTS_DIR
 
 
 AZURE_CONVERTER_KEY = os.environ.get("AZURE_CONVERTER_KEY")
@@ -20,35 +17,9 @@ def convert_all_pdfs():
 
     Saves the json outputs to the output directory.
     """
-    converter = AzureConverter(
-        endpoint="https://azureconverter.cognitiveservices.azure.com/",
-        credential_key=AZURE_CONVERTER_KEY,
-        model_id="prebuilt-layout",  # Was "prebuilt-document"
-        save_json=True,
-    )
-
-    for pdf_path in Path(PDF_REPORTS_DIR).rglob("*.pdf"):
-        # Check if the output file already exists
-        output_path = Path(JSON_REPORTS_DIR) / (pdf_path.stem + ".json")
-        if output_path.exists():
-            logger.info(f"Skipping {pdf_path} as output already exists")
-            continue
-
-        # Convert the pdf to json
-        logger.info(f"Converting {pdf_path} to json")
-        try:
-            converter.convert(file_path=pdf_path)
-        except Exception as exc:
-            logger.error(f"Failed to convert {pdf_path} to json: {exc}")
-            continue
-
-        # Move the output file to the output directory
-        try:
-            os.rename(pdf_path.with_suffix(".json"), output_path)
-        except Exception as exc:
-            logger.error(f"Failed to move {pdf_path} to {output_path}: {exc}")
-
-    logger.info("Finished")
+    # Loop over the pdf filenames in the source directory
+    for pdf_filename in os.listdir(PDF_REPORTS_DIR):
+        convert_report(pdf_filename)
 
 
 if __name__ == "__main__":
